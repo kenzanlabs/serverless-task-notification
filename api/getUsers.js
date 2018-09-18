2fc0902dc0b5ac53533ebc1f2ea90aebccd2c5c8
Jacob Nerney <jnerney@kenzan.com>
2018-09-18 12:55:33 -0400

    getUsers lambda

--- api/getUsers.js
+++ api/getUsers.js
@@ -1,2 +1,23 @@
 "use strict";
-module.exports.get = {};
+
+const aws = require("aws-sdk");
+const dynamo = new aws.DynamoDB.DocumentClient();
+
+module.exports.get = (event, context, callback) => {
+  dynamo.scan({ TableName: "users" }, (err, result) => {
+    if (err) {
+      console.log(err);
+      callback(null, {
+        statusCode: err.statusCode || 500,
+        headers: { "Content-Type": "text/plain" },
+        body: "error getting users"
+      });
+      return;
+    }
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items)
+    };
+    callback(null, response);
+  });
+};
