881723374faedf1af563654877d155e421487723
Luis Hernández Castillo <luis.hdz1010@gmail.com>
2018-09-20 10:45:08 -0500

    Migrates app to typescript (#9)
    * migrates app to typescript
    
    * runs prettier
    
    * fixes tests
    
    * removes js files
    
    * merges local gitignore with root's

---
+++ app/src/components/TextInput/TextInput.stories.tsx
@@ -0,0 +1,5 @@
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+import TextInput from './TextInput'
+
+storiesOf('TextInput', module).addWithJSX('default', () => <TextInput />)
---
+++ app/typings/styled-components.d.ts
@@ -0,0 +1,18 @@
+import {
+  SimpleInterpolation,
+  ThemedStyledComponentsModule,
+} from 'styled-components'
+
+declare module 'styled-components' {
+  export interface ThemedStyledComponentsModule<T> {
+    createGlobalStyle(
+      strings: TemplateStringsArray,
+      ...interpolations: SimpleInterpolation[]
+    ): React.ComponentClass
+  }
+
+  export function createGlobalStyle(
+    strings: TemplateStringsArray,
+    ...interpolations: SimpleInterpolation[]
+  ): React.ComponentClass
+}
