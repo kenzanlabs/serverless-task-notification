f5d48a1e4d02c582ea323826a50f0061ac8c1f49
Gogs <gogs@fake.local>
2018-09-20 23:11:34 -0400

    adding first test

--- api/test/test.js
+++ api/test/test.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const lTester = require("lambda-tester");
+const getUsers = require("../users/getUsers").get;
+
+describe("getUsers", function() {
+  it("should get all users", function() {
+    return lTester( getUsers )
+      .expectResult();
+  });
+});  
