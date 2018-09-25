e591ac1bda37ee57c06fc74d4ba8425f88652d39
Carlos Hernandez <rhernandez@kenzan.com>
2018-09-25 14:46:43 -0400

    simple socket.io server

--- app-server/server.js
+++ app-server/server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const http = require('http');
 
+const socketIO = require('socket.io')
+
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server)
 
 const PORT = process.env.PORT || 9000;
 
-app.use("/", (req, res) => {
-  res.send("Connected!");
+async function notificationHandler(req, res) {
+  const response = await 'Connected';
+
+  res.send(response);
+}
+
+io.on('connection', socket => {
+  console.log('User connected')
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  });
 });
 
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.use("/notifications", notificationHandler);
+
+server.listen(PORT, (err) => {
+  if(err) return console.log(err);
+
+  return console.log(`Listening on port ${PORT}`)
+});
