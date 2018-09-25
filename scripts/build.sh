33b0a69167670dad226c256ed648a2bb3da9daca
Jacob Nerney <nerney@users.noreply.github.com>
2018-09-25 15:41:25 -0400

    fixing build (#15)

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+npm install
+cd app && npm install
+npm run build
+cd ..
+
+export CODEBUILD_GIT_BRANCH="$(git symbolic-ref HEAD --short 2>/dev/null)"
+if [ "$CODEBUILD_GIT_BRANCH" = "" ] ; then
+  CODEBUILD_GIT_BRANCH="$(git branch -a --contains HEAD 2>/dev/null | sed -n 2p | awk '{ printf $1 }')";
+  export CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/};
+fi
+
+if [ "$CODEBUILD_GIT_BRANCH" = "master" ] ; then
+  serverless deploy && node ./api/test/populateUsers.js
+fi
