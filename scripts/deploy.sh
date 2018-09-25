c7f75026da41793368bcf1757e6dbdf37869b94b
Jacob Nerney <jnerney@kenzan.com>
2018-09-25 17:53:14 -0400

    sanity check

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,9 @@
 cd api/users
-echo "\nDeploying users service"
 serverless deploy
 cd ../tasks
-echo "\nDeploying tasks service"
 serverless deploy
 cd ../notifications
-echo "Deploying notifications service"
 serverless deploy
 cd ../..
-echo "\nDeploying FE app"
 serverless client deploy --no-confirm
 node ./api/users/populate.js
