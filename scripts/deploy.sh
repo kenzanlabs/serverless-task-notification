8e4270d66d6670ec24129d8f0428f575185bd59b
Jacob Nerney <nerney@users.noreply.github.com>
2018-10-02 11:47:35 -0400

    fix script now good (#22)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-cd api/users
+cd app
+serverless deploy
+
+cd ../socket-server
+serverless deploy
+
+cd ../api/users
 serverless deploy 
 cd ../tasks
 serverless deploy
 cd ../notifications
 serverless deploy
-cd ../../app
-serverless deploy
-#cd ../socket-server
-#serverless deploy
+
