3921618e25514d51266f6487f75e3d8e51efdfc9
Jacob Nerney <jnerney@kenzan.com>
2018-10-02 16:05:47 -0400

    saving some commands

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -11,3 +11,11 @@ git clone https://github.com/kenzanlabs/serverless-task-notification.git
 cd serverless-task-notification/socket-server
 docker build -t socket-server .
 docker run -d -p 9000:9000 socket-server
+
+# restart with changes
+cd serverless-task-notification
+git pull
+cd socket-server
+sudo service docker restart
+docker build -t socket-server .
+docker run -d -p 9000:9000 socket-server
