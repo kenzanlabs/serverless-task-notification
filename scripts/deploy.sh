fd04ddef331bba45f32c3352c1bc979364ac1683
Jacob Nerney <jnerney@kenzan.com>
2018-10-03 17:07:16 -0400

    all good

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,9 +18,3 @@ serverless deploy --aws-s3-accelerate -v
 # FE build and deploy
 cd ../../app
 serverless deploy --aws-s3-accelerate -v
-
-# Redeploy socket-server
-echo "$EC2_KEY" > serverless-task-notification.pem
-chmod 400 serverless-task-notification.pem
-export EC2_DNS=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=socketServer' --query 'Reservations[0].Instances[0].PublicDnsName') && export EC2_DNS="${EC2_DNS//\"/}"
-ssh -o StrictHostKeyChecking=no -i "serverless-task-notification.pem" ec2-user@$EC2_DNS 'bash -s' < socket-server/redeploy.sh
