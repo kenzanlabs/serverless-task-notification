abb042cb8ece701bce1b1e0ae22ff9e4803b2327
Jacob Nerney <nerney@users.noreply.github.com>
2018-10-03 13:02:51 -0400

    export DNS of ec2 instance (#32)
    * export DNS of ec2 instance
    
    * scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,17 +5,17 @@ npm install -g serverless
 
 # Provision EC2 instance, if needed
 cd socket-server
-serverless deploy --aws-s3-accelerate
+serverless deploy --aws-s3-accelerate -v 
 
 # Get in formation, lambdas!
 cd ../api/users
-serverless deploy --aws-s3-accelerate
+serverless deploy --aws-s3-accelerate -v 
 cd ../tasks
-serverless deploy --aws-s3-accelerate
+serverless deploy --aws-s3-accelerate -v 
 cd ../notifications
-serverless deploy --aws-s3-accelerate
+serverless deploy --aws-s3-accelerate -v 
 
 # FE build and deploy
 cd ../../app
-serverless deploy --aws-s3-accelerate
+serverless deploy --aws-s3-accelerate -v
 
