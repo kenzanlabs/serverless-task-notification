5f2635569589024ce302e1bf9f03d63b791bcb19
Jacob Nerney <nerney@users.noreply.github.com>
2018-10-03 12:21:11 -0400

    build things (#30)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,17 +5,17 @@ npm install -g serverless
 
 # Provision EC2 instance, if needed
 cd socket-server
-serverless deploy
+serverless deploy --aws-s3-accelerate
 
 # Get in formation, lambdas!
 cd ../api/users
-serverless deploy 
+serverless deploy --aws-s3-accelerate
 cd ../tasks
-serverless deploy
+serverless deploy --aws-s3-accelerate
 cd ../notifications
-serverless deploy
+serverless deploy --aws-s3-accelerate
 
 # FE build and deploy
 cd ../../app
-serverless deploy
+serverless deploy --aws-s3-accelerate
 
