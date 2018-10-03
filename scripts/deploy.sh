d1ffb677fa657f862f918607408e29464b7a50e8
Jacob Nerney <jnerney@kenzan.com>
2018-10-03 16:49:18 -0400

    dns now not hard coded

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,6 +22,5 @@ serverless deploy --aws-s3-accelerate -v
 # Redeploy socket-server
 echo "$EC2_KEY" > serverless-task-notification.pem
 chmod 400 serverless-task-notification.pem
-ssh -o StrictHostKeyChecking=no -i "serverless-task-notification.pem" ec2-user@ec2-34-229-16-49.compute-1.amazonaws.com 'bash -s' < socket-server/redeploy.sh
-      
-      #aws ec2 describe-instances --filters 'Name=tag:Name,Values=socketServer' --query  'Reservations[0].Instances[0].PublicDnsName'
+export EC2_DNS=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=socketServer' --query 'Reservations[0].Instances[0].PublicDnsName') && export EC2_DNS="${EC2_DNS//\"/}"
+ssh -o StrictHostKeyChecking=no -i "serverless-task-notification.pem" ec2-user@$EC2_DNS 'bash -s' < socket-server/redeploy.sh
