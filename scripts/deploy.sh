0318629f1e3e97f88ec11d10fb4e81b19554853a
Jacob Nerney <jnerney@kenzan.com>
2018-10-03 16:36:54 -0400

    ssh things

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,3 +19,9 @@ serverless deploy --aws-s3-accelerate -v
 cd ../../app
 serverless deploy --aws-s3-accelerate -v
 
+# Redeploy socket-server
+echo "$EC2_KEY" > serverless-task-notification.pem
+chmod 400 serverless-task-notification.pem
+ssh -o StrictHostKeyChecking=no -i "serverless-task-notification.pem" ec2-user@ec2-34-229-16-49.compute-1.amazonaws.com 'bash -s' < socket-server/redeploy.sh
+      
+      #aws ec2 describe-instances --filters 'Name=tag:Name,Values=socketServer' --query  'Reservations[0].Instances[0].PublicDnsName'
