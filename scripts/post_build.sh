ace74a915a55ea3042dffb9537631020c1244d7e
Jacob Nerney <nerney@users.noreply.github.com>
2018-10-03 17:25:41 -0400

    Ssh stuff (#37)
    * ssh things
    
    * dns now not hard coded
    
    * all good
    
    * more
    
    * good

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -12,7 +12,7 @@ if [ "$CODEBUILD_GIT_BRANCH" = "master" ] ; then
   bash scripts/deploy.sh
 
   # Redeploy socket-server
-  echo "$EC2_KEY" > serverless-task-notification.pem
+  echo -e "$EC2_KEY" > serverless-task-notification.pem
   chmod 400 serverless-task-notification.pem
   export EC2_DNS=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=socketServer' --query 'Reservations[0].Instances[0].PublicDnsName') && export EC2_DNS="${EC2_DNS//\"/}"
   ssh -o StrictHostKeyChecking=no -i "serverless-task-notification.pem" ec2-user@$EC2_DNS 'bash -s' < socket-server/redeploy.sh
